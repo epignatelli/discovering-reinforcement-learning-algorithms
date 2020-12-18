f83c96e5d69ba9656c4b31e607d7f2a0e2597b25
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2020-12-18 11:21:14 +0100

    add gru

--- lpg/modules.py
+++ lpg/modules.py
@@ -74,34 +74,69 @@ def LSTM(
     c_initial_state=zeros,
     initial_state_seed=0,
 ):
-    """"""
-    cell = LSTMCell(
-        hidden_size,
-        W_init,
-        b_init,
-        h_initial_state,
-        c_initial_state,
-        initial_state_seed,
+    return Rnn(
+        LSTMCell(
+            hidden_size,
+            W_init,
+            b_init,
+            h_initial_state,
+            c_initial_state,
+            initial_state_seed,
+        )
     )
-    return Rnn(cell)
 
 
+@rnn_cell
 def GRUCell(
     hidden_size,
     W_init=glorot_normal(),
     b_init=normal(),
-    h_initial_state=zeros,
-    c_initial_state=zeros,
+    initial_state_fn=zeros,
     initial_state_seed=0,
 ):
     """Layer construction function for an GRU cell.
     Formulation: Chun, J., 2014, https://arxiv.org/pdf/1412.3555v1.pdf"""
 
     def initial_state():
-        pass
+        rng = jax.random.PRNGKey(initial_state_seed)
+        return initial_state_fn(rng, (hidden_size,))
 
     def init(rng, input_shape):
-        pass
+        in_dim, out_dim = input_shape[-1] + hidden_size, 3 * hidden_size
+        k1, k2 = jax.random.split(rng)
+        W_i = W_init(k1, (in_dim, out_dim))
+        W_h = W_init(k1, (in_dim, out_dim))
+        b = b_init(k1, (out_dim,))
+        output_shape = input_shape[:-1] + (hidden_size,)
+        return output_shape, (W_i, W_h, b)
 
     def apply(params, inputs, prev_state=initial_state()):
-        pass
+        W_i, W_h, b = params
+        W_hz, W_ha = jnp.split(W_h, indices_or_sections=(2 * hidden_size,), axis=-1)
+        b_z, b_a = jnp.split(b, indices_or_sections=(2 * hidden_size,), axis=-1)
+
+        gated = jnp.matmul(inputs, W_i)
+        zr_x, a_x = jnp.split(gated, indices_or_sections=[2 * hidden_size], axis=-1)
+        zr_h = jnp.matmul(prev_state, W_hz)
+        z, r = jnp.split(
+            jax.nn.sigmoid(zr_x + zr_h + b_z), indices_or_sections=2, axis=-1
+        )
+        a_h = jnp.matmul(r * prev_state, W_ha)
+        a = jnp.tanh(a_x + a_h + jnp.broadcast_to(b_a, a_h.shape))
+        h = (1 - z) * prev_state + z * a
+        return h, h
+
+    return (initial_state, init, apply)
+
+
+@module
+def GRU(
+    hidden_size,
+    W_init=glorot_normal(),
+    b_init=normal(),
+    initial_state_fn=zeros,
+    initial_state_seed=0,
+):
+    return Rnn(
+        GRUCell(hidden_size, W_init, b_init, initial_state_fn, initial_state_seed)
+    )
--- test/modules_test.py
+++ test/modules_test.py
@@ -30,3 +30,32 @@ def test_lstm():
 
 
 test_lstm()
+
+
+def test_gru_cell():
+    rng = jax.random.PRNGKey(0)
+    input_shape = (8,)
+    gru = LSTMCell(16)
+    x = jax.random.normal(rng, input_shape)
+    out_shape, params = gru.init(rng, input_shape)
+    outputs, state = gru.apply(params, x)
+    print(outputs.shape, state.h.shape, state.c.shape)
+
+
+test_gru_cell()
+
+
+def test_gru():
+    rng = jax.random.PRNGKey(0)
+    SEQ_LEN = 5
+    INPUT_FEATURES = 8
+    HIDDEN_SIZE = 16
+    input_shape = (SEQ_LEN, INPUT_FEATURES)
+    gru = LSTM(HIDDEN_SIZE)
+    x = jax.random.normal(rng, input_shape)
+    out_shape, params = gru.init(rng, input_shape)
+    outputs, hidden_state = gru.apply(params, x)
+    print(outputs.shape)
+
+
+test_gru()
